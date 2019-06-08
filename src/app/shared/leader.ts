65cc89542c13e185fddfecee7b80dbb538e2bcdf
ElGambino <work.samyakjain@gmail.com>
2019-06-08 15:11:21 +0530

    Assignment 2

---
+++ src/app/shared/leader.ts
@@ -0,0 +1,11 @@
+export class Leader {
+// tslint:disable-next-line: indent
+	id: string;
+    name: string;
+    image: string;
+// tslint:disable-next-line: indent
+	featured: boolean;
+    description: string;
+    abbr: string;
+    designation: string;
+}
